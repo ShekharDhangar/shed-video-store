bb8df9b8be6057d9843907658fdbd4e7944e1567
Shekhar Dhangar <shekhardhangar@yahoo.com>
2022-03-30 00:41:11 +0530

    Refactored code

--- src/pages/AuthenticationPage/AuthenticationComp/AuthSignUp/AuthSignUp.jsx
+++ src/pages/AuthenticationPage/AuthenticationComp/AuthSignUp/AuthSignUp.jsx
@@ -27,27 +27,20 @@ function AuthSignUp() {
     password: "",
     password2: "",
   });
-// passwordValidation is not being used currently but will be used in future.
-  const passwordValidation = () => {
-    const passwordExpression =
-    /^(?=.*\d)(?=.*[a-z]).{5,10}$/;
 
-    if (inputState.password !== inputState.password2) {
-      console.log("passwords do not match");
-      return false;
-    }
-    if (!passwordExpression.test(inputState.password) || !inputState.password2.match(passwordExpression)){
-      console.log("password should follow rules");
-      return false
-    }
-    return true;
-  };
-
-  
+  const passwordExpression =
+  /^(?=.*\d)(?=.*[a-z]).{5,10}$/;
 
   function submitFormHandler(e) {
     e.preventDefault();
+    const passwordMatch = (inputState.password !== inputState.password2);
+    const passwordExpressionMatch = (!passwordExpression.test(inputState.password) || !inputState.password2.match(passwordExpression))  
+    if(!passwordMatch && !passwordExpressionMatch){
       signUpUser(inputState,setformSubmitState);;
+    }else{
+      console.log('password must cintainer more than 5 characters with number,alphabet and atleast 1 special character')
+    }
+    return true;
   }
   return (
     <>
